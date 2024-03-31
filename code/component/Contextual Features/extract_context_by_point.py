f36e20fcf484e6bf3318f54751924c5690974f49
daqianshijie2000 <97569608+daqianshijie2000@users.noreply.github.com>
2024-03-31 14:56:10 -0400

    Update extract_context_by_point.py

--- code/component/Contextual Features/extract_context_by_point.py
+++ code/component/Contextual Features/extract_context_by_point.py
@@ -7,11 +7,27 @@ from concurrent.futures import ProcessPoolExecutor
 
 
 def process_tif(tif_file, lagos_poly, output_dir):
+    """
+    Processes a given TIFF file by extracting information based on a provided geospatial polygon,
+    and saves the extracted information to a CSV file.
 
+    Args:
+    tif_file (str): The path to the TIFF file to be processed.
+    lagos_poly (GeoDataFrame): A GeoDataFrame containing the polygons to extract data within.
+                                The geometry column should contain the polygons' centroids.
+    output_dir (str): The directory path where the output CSV files will be saved.
+
+    Returns:
+    DataFrame: A pandas DataFrame containing the extracted information.
+    """
+
+    # Ensure the output directory exists
     os.makedirs(output_dir, exist_ok=True)
 
+    # Open the TIFF file for reading
     with gw.open(tif_file) as src:
-        # Extract data for the resampled contextual TIFF file
+
+        # Extract data for the resampled contextual TIFF file within the provided polygon, handling nodata values
         df = gw.extract(src,
                         lagos_poly,
                         nodata=-9999)
@@ -22,7 +38,7 @@ def process_tif(tif_file, lagos_poly, output_dir):
         # Construct the output CSV file path
         output_csv_path = os.path.join(output_dir, f"{feature_name}_centroid.csv")
 
-        # Save the DataFrame to the CSV file in the specified folder
+        # Save the extracted data to the CSV file
         df.to_csv(output_csv_path, index=False)
 
     return df
@@ -30,16 +46,20 @@ def process_tif(tif_file, lagos_poly, output_dir):
 
 if __name__ == "__main__":
     gpkg_path = '/home/ubuntu/Cap2024/CapstoneDataset2024/Capstone_2024/Reference/Deprived_Areas/100mGrid_Lagos.gpkg'
+    # Read the geospatial plygons from the GeoPackage
     lagos_poly = gpd.read_file(gpkg_path)
+    # Calculate the centroids of the polygons
     lagos_poly["geometry"] = lagos_poly.centroid
 
+    # Directory containing the TIFF files to be processed
     tif_directory = '/home/ubuntu/Cap2024/context/resampled_data'
+    # Retrieve all TIFF file paths in the specified directory
     tif_files = glob.glob(os.path.join(tif_directory, '*.tif'))
 
     # Define the output folder where the CSV files will be saved
     output_dir = '/home/ubuntu/Cap2024/context/contextual_features_extraction'
 
-    # Process TIFF files in parallel and save CSVs in the specified folder
+    # Process each TIFF file in parallel, extract information and save each CSV in the specified folder
     with ProcessPoolExecutor() as executor:
         futures = [executor.submit(process_tif, tif_file, lagos_poly, output_dir) for tif_file in tif_files]
 
