139cc3092d39279acfe35721ceb2894503156093
daqianshijie2000 <97569608+daqianshijie2000@users.noreply.github.com>
2024-03-31 14:58:24 -0400

    Update extract_by_point.py

--- code/component/Covariate/extract_by_point.py
+++ code/component/Covariate/extract_by_point.py
@@ -1,24 +1,22 @@
-# %%
 import geopandas as gpd
 import geowombat as gw
 
-# %%
+# Read a GeoPackage containing polygons
 lagos_poly = gpd.read_file("100mGrid_Lagos.gpkg")
+
+# Replace the geometry of each polygon with its centroid
 lagos_poly["geometry"] = lagos_poly.centroid
 
-# %%
-
+# Open a raster file that contains covariate data for Lagos with 53 bands
 with gw.open(
     "lag_covariate_compilation_53bands.tif",
-    # band_names=[""]
 ) as src:
-
+    # Extract raster values using the centroids of the polygons defined in 'lagos_poly'
     df = gw.extract(
         src,
         lagos_poly,
         nodata=-9999,
     )
-
+    
+    # Save the extracted data to a CSV file
     df.to_csv("lagos_centroid.csv", index=False)
-
-# %%
