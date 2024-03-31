98eab7c3dbfdc8eba3407ef8d205420c7d507355
daqianshijie2000 <97569608+daqianshijie2000@users.noreply.github.com>
2024-03-31 15:00:16 -0400

    Update coordinate_extraction.py

--- code/component/Raw Image Processing/coordinate_extraction.py
+++ code/component/Raw Image Processing/coordinate_extraction.py
@@ -1,53 +1,24 @@
-import sys
-import os
-import rasterio
-import pandas as pd
-import numpy as np
-import logging
+import geopandas as gpd
+import geowombat as gw
 
-def extract_coordinates_and_labels(file_path):
-    """
-    Extracts coordinates and labels from a TIFF file.
-    Args:
-        file_path (str): Path to the TIFF file.
-    Returns:
-        DataFrame: Contains longitude, latitude, and labels.
-    """
-    try:
-        with rasterio.open(file_path) as dataset:
-            val = dataset.read(1)
-            no_data = dataset.nodata
+# Read a GeoPackage containing polygons
+lagos_poly = gpd.read_file("100mGrid_Lagos.gpkg")
+# Replace the geometry of each polygon with its centroid
+lagos_poly["geometry"] = lagos_poly.centroid
 
-            data = [(dataset.xy(x, y)[0], dataset.xy(x, y)[1], val[x, y])
-                    for x, y in np.ndindex(val.shape) if val[x, y] != no_data]
+# Open a raster file that contains a resampled tif file with lagos_bgrn
+with gw.open(
+    "lag_bgrn_resampled_file.tif",
+) as src:
+    # Extract raster values using the centroids of the polygons defined in 'lagos_poly'
+    df = gw.extract(
+        src,
+        lagos_poly,
+        nodata=-9999,
+    )
 
-            lon = [i[0] for i in data]
-            lat = [i[1] for i in data]
-            labels = [i[2] for i in data]
-            df = pd.DataFrame({"longitude": lon, "latitude": lat, "label": labels})
+    # Rename columns in the extracted DataFrame
+    df.rename(columns={1: 'bgrn_1', 2: 'bgrn_2', 3: 'bgrn_3', 4: 'bgrn_4'}, inplace=True)
 
-            return df
-
-    except Exception as e:
-        logging.error(f'Error extracting coordinates and labels from {file_path}: {e}')
-        return None
-
-if __name__ == "__main__":
-    try:
-        file_path = '/home/ubuntu/Cap2024/train/lag_bgrn_resampled_file.tif'
-        output_file = '/home/ubuntu/Cap2024/train/extracted_data.csv'
-
-        # Ensure output directory exists
-        output_dir = os.path.dirname(output_file)
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-
-        # Extract coordinates and labels from the TIFF file
-        df = extract_coordinates_and_labels(file_path)
-        if df is not None:
-            df.to_csv(output_file, index=False)
-            logging.info(f"Data extracted and saved to {output_file}")
-
-    except Exception as e:
-        logging.error(f"Error in main function: {e}")
-        sys.exit(1)
+    # Save the extracted data to a CSV file
+    df.to_csv("lagos_bgrn_resampled.csv", index=False)
