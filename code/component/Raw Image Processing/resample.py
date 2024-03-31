8ce387718729804222f5237192de72a14514d05d
daqianshijie2000 <97569608+daqianshijie2000@users.noreply.github.com>
2024-03-31 14:59:37 -0400

    Update resample.py

--- code/component/Raw Image Processing/resample.py
+++ code/component/Raw Image Processing/resample.py
@@ -5,11 +5,26 @@ from rasterio.enums import Resampling
 import logging
 
 def read_and_resample_raster(file_path, output_path, resampling_factor, resampling_method=Resampling.bilinear):
+    """
+        Read a raster file, resample it, and save the resampled raster to a new file.
+
+        Args:
+            file_path (str): Path to the original raster file.
+            output_path (str): Path where the resampled raster will be saved.
+            resampling_factor (float): Factor by which the raster's resolution is changed.
+            resampling_method (Resampling): Method used for resampling. Default is bilinear interpolation.
+
+        The function opens the original raster file, calculates the new dimensions based on the resampling factor,
+        and uses these dimensions to resample the raster data. The resampled raster is then saved to a new file at the
+        specified output path.
+        """
     try:
         with rasterio.open(file_path) as dataset:
+            # Calculate new dimensions
             new_height = int(dataset.height * resampling_factor)
             new_width = int(dataset.width * resampling_factor)
 
+            # Resample the raster to the new dimensions
             resampled_data = dataset.read(
                 out_shape=(
                     dataset.count,
@@ -19,13 +34,15 @@ def read_and_resample_raster(file_path, output_path, resampling_factor, resampli
                 resampling=resampling_method
             )
 
+            # Get data type of the resampled raster
             dtype = resampled_data.dtype
 
+            # Calculate the new transformation for the resampled raster
             transform = dataset.transform * dataset.transform.scale(
                 (dataset.width / new_width),
                 (dataset.height / new_height)
             )
-
+            # Save the resampled raster to a new file
             with rasterio.open(
                 output_path,
                 'w',
@@ -46,9 +63,10 @@ def read_and_resample_raster(file_path, output_path, resampling_factor, resampli
 
 if __name__ == "__main__":
     try:
+        # Define the path to the original and the output resampled raster files
         file_path = '/home/ubuntu/Cap2024/train/lag_bgrn.tif'
         output_path = '/home/ubuntu/Cap2024/train/lag_bgrn_resampled_file.tif'
-        resampling_factor = 0.1  # Adjust as needed
+        resampling_factor = 0.1  # Define the resampling factor, here set to reduce the resolution to 10%
 
         # Ensure output directory exists
         output_dir = os.path.dirname(output_path)
