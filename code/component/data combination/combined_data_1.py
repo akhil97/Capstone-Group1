7d4b1f8a288bf2d771a10d1aa88021cf8cdfb319
daqianshijie2000 <97569608+daqianshijie2000@users.noreply.github.com>
2024-04-02 23:46:05 -0400

    Update combined_data_1.py

--- code/component/data combination/combined_data_1.py
+++ code/component/data combination/combined_data_1.py
@@ -2,14 +2,15 @@ import pandas as pd
 
 def merge_csvs_on_geometry(covariate_file, merged_contextual_feature_file, bgrn_file, output_file):
     """
-        Merges two CSV files based on a common 'geometry' column and saves the merged data to a new CSV file.
+        Merges three CSV files based on a common 'geometry' column and saves the merged data to a new CSV file.
 
         Args:
             merged_contextual_feature_file (str): File path for the CSV containing the contextual features data.
-            covariate_file (str): File path for the CSV containing the covariate data.
+            covariate_file (str): File path for the CSV containing the covariate data and slum lables.
+            bgrn_file (str): File path for the CSV containing four bgrn data.
             output_file (str): File path where the merged data will be saved as a new CSV file.
 
-        The function reads the two specified CSV files into pandas DataFrames, then merges them using
+        The function reads the three specified CSV files into pandas DataFrames, then merges them using
         pandas' merge function on the 'geometry' column. The merge is performed as an 'outer' join,
         ensuring that all entries from both tables are included in the result, with missing values filled with NaNs
         where no matching geometry is found. The merged DataFrame is then saved to the specified output file path.
@@ -21,7 +22,6 @@ def merge_csvs_on_geometry(covariate_file, merged_contextual_feature_file, bgrn_
 
     # Select only the 'geometry' column and the bgrn band values from the bgrn DataFrame
     df_bgrn = df_bgrn[['geometry', 'bgrn_1', 'bgrn_2', 'bgrn_3', 'bgrn_4']]
-
 
     # Merge the two DataFrames on the 'geometry' column using an 'outer' join
     df_merged_1 = pd.merge(df_covariate, df_contextual_feature, on='geometry', how='outer')
