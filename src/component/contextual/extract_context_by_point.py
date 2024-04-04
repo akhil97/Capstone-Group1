e7e9dd4eb9f4eb9cf6b3ef4b37c44b657327b736
daqianshijie2000 <97569608+daqianshijie2000@users.noreply.github.com>
2024-04-04 15:59:06 -0400

    Update extract_context_by_point.py

--- src/component/contextual/extract_context_by_point.py
+++ src/component/contextual/extract_context_by_point.py
@@ -10,7 +10,34 @@ class TIFFProcessor:
         self.gpkg_path = gpkg_path
         self.tif_directory = tif_directory
         self.output_dir = output_dir
+        self.lagos_poly = self.read_and_extract_polygons()
 
+    def read_and_extract_polygons(self)
+        # Read the geospatial polygons from the GeoPackage
+        lagos_poly = gpd.read_file(self.gpkg_path)
+
+        # Calculate the centroids of the polygons
+        lagos_poly["geometry"] = lagos_poly.centroid
+
+        return lagos_poly
+        
+    def get_tiff_files(self, base_dir):
+        """
+        Get TIFF files from subdirectories.
+        
+        Args:
+            base_dir (str): Starting directory path.
+        
+        Returns:
+            List of paths to TIFF files found within `base_dir` and its subdirectories.
+        """
+        tiff_files = []
+        for dirpath, _, filenames in os.walk(base_dir):
+            for filename in filenames:
+                if filename.endswith('.tif'):
+                    tiff_files.append(os.path.join(dirpath, filename))
+        return tiff_files
+        
     def process_tif(self, tif_file):
         """
         Processes a given TIFF file by extracting information based on a provided geospatial polygon,
@@ -18,43 +45,34 @@ class TIFFProcessor:
 
         Args:
         tif_file (str): The path to the TIFF file to be processed.
-
-        Returns:
-        DataFrame: A pandas DataFrame containing the extracted information.
         """
-
-        # Read the geospatial polygons from the GeoPackage
-        lagos_poly = gpd.read_file(self.gpkg_path)
-
-        # Calculate the centroids of the polygons
-        lagos_poly["geometry"] = lagos_poly.centroid
-
         # Ensure the output directory exists
         os.makedirs(self.output_dir, exist_ok=True)
 
+        try: 
         # Open the TIFF file for reading
-        with gw.open(tif_file) as src:
-            # Extract data for the resampled contextual TIFF file within the provided polygon, handling nodata values
-            df = gw.extract(src,
-                            lagos_poly,
-                            nodata=-9999)
+            with gw.open(tif_file) as src:
+                # Extract data for the resampled contextual TIFF file within the provided polygon, handling nodata values
+                df = gw.extract(src,
+                                self.lagos_poly,
+                                nodata=-9999)
 
-            # Extract the feature name
-            feature_name = os.path.splitext(os.path.basename(tif_file))[0]
+                # Extract the feature name
+                feature_name = os.path.splitext(os.path.basename(tif_file))[0]
 
-            # Construct the output CSV file path
-            output_csv_path = os.path.join(self.output_dir, f"{feature_name}_centroid.csv")
+                # Construct the output CSV file path
+                output_csv_path = os.path.join(self.output_dir, f"{feature_name}_centroid.csv")
 
-            # Save the extracted data to the CSV file
-            df.to_csv(output_csv_path, index=False)
-
-        return df
+                # Save the extracted data to the CSV file
+                df.to_csv(output_csv_path, index=False)
+                
+        except Exception as e:
+            print(f"Error processing {tif_file}: {e}")
 
     def process_all_tifs(self):
         # Retrieve all TIFF file paths in the specified directory
-        tif_files = glob.glob(os.path.join(self.tif_directory, '*.tif'))
+        tif_files = self.get_tiff_files(self.tif_directory)
 
-        # Process each TIFF file in parallel, extract information and save each CSV in the specified folder
         with ProcessPoolExecutor() as executor:
             futures = [executor.submit(self.process_tif, tif_file) for tif_file in tif_files]
 
