1eb38e57b15b93308c443f086e3aabec1f83ad12
daqianshijie2000 <97569608+daqianshijie2000@users.noreply.github.com>
2024-04-08 11:41:47 -0400

    Add files via upload

--- src/component/covariate/extract_by_point.py
+++ src/component/covariate/extract_by_point.py
@@ -1,5 +1,8 @@
 import geopandas as gpd
 import geowombat as gw
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class Extractor:
     def __init__(self, poly_file, raster_file, output_file):
@@ -8,20 +11,28 @@ class Extractor:
         self.output_file = output_file
 
     def extract_data(self):
-        # Read a GeoPackage containing polygons and slum labels
-        lagos_poly = gpd.read_file(self.poly_file)
+        logging.info(f'Starting data extraction...')
+        try:
+            # Read a GeoPackage containing polygons and slum labels
+            lagos_poly = gpd.read_file(self.poly_file)
 
-        # Replace the geometry of each polygon with its centroid
-        lagos_poly["geometry"] = lagos_poly.centroid
+            # Replace the geometry of each polygon with its centroid
+            lagos_poly["geometry"] = lagos_poly.centroid
 
-        # Open a raster file that contains covariate data for Lagos with 53 bands
-        with gw.open(self.raster_file) as src:
-            # Extract raster values using the centroids of the polygons defined in 'lagos_poly'
-            df = gw.extract(
-                src,
-                lagos_poly,
-                nodata=-9999,
-            )
+            # Open a raster file that contains covariate data for Lagos with 53 bands
+            with gw.open(self.raster_file) as src:
+                logging.info(f'Raster file {self.raster_file} opened successfully.')
+                # Extract raster values using the centroids of the polygons defined in 'lagos_poly'
+                df = gw.extract(
+                    src,
+                    lagos_poly,
+                    nodata=-9999,
+                )
+                logging.info('Data extraction from raster file completed.')
 
-            # Save the extracted data to a CSV file which has included slum labels
-            df.to_csv(self.output_file, index=False)
+                # Save the extracted data to a CSV file which has included slum labels
+                df.to_csv(self.output_file, index=False)
+                logging.info(f'Covariate CSV saved successfully to {self.output_file}')
+
+        except Exception as e:
+            logging.error(f'Error during extraction: {e}')
