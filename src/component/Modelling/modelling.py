116254095513ad1434b895b00da6238464e229e0
akhil97 <akhilm97@gmail.com>
2024-04-09 21:58:30 -0400

    Removed no data -9999 rows from the dataset

--- src/component/Modelling/modelling.py
+++ src/component/Modelling/modelling.py
@@ -1,12 +1,8 @@
 import pandas as pd
-from sklearn.neural_network import MLPClassifier
-from pycaret.datasets import get_data
 from pycaret.classification import *
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
 
-data = pd.read_csv('../../../Data/lagos_input.csv')
+data = pd.read_csv('../../../Data/final_output_lagos.csv')
 data['Slum'].fillna(0, inplace=True)
 
 # Changing label values from 1.0, 2.0 to 1
@@ -14,83 +10,33 @@ label_encoder = LabelEncoder()
 data['Slum'] = label_encoder.fit_transform(data['Slum'])
 data['Slum'] = data['Slum'].replace([1, 2], 1)
 
+data3 = data[data['Slum'] == 3]
+
 #Removing Slum label 3 since it is unsure
 data = data[data['Slum'] != 3]
 
 data = data.drop(['geometry', 'id'], axis = 1)
 
-X = data.iloc[:, 1:]
-y = data['Slum']
-
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state=1)
-clf = MLPClassifier(random_state=1, max_iter=300).fit(X_train, y_train)
-predictions = clf.predict(X_test)
-print(classification_report(y_test, predictions))
-
+#X = data.iloc[:, 1:]
+#y = data['Slum']
+data = data[~data.isin([-9999]).any(axis=1)]
 clf1 = setup(data, target = 'Slum')
-best_model = compare_models()
-
-#Logistic Regression
-lr = create_model('lr')
-predict_model(lr)
-
-#Random Forest
-rf = create_model('rf')
-predict_model(rf)
-
-#K Nearest Neighbors
-knn = create_model('knn')
-predict_model(knn)
+best_model = compare_models(include = ['xgboost'])
 
 #XGBoost
 xgboost = create_model('xgboost')
 predict_model(xgboost)
 
-#MLP
-mlp = create_model('mlp')
-predict_model(mlp)
+#Tune best model
+tuned_model = tune_model(xgboost)
 
-#Decision Trees
-dt = create_model('dt')
-predict_model(dt)
+#AUC-ROC plot
+plot_model(estimator = tuned_model, plot = 'auc')
 
+#Plotting the confusion Matrix
+plot_model(estimator = tuned_model, plot = 'confusion_matrix')
 
-#Ridge Classifier
-ridge = create_model('ridge')
-predict_model(ridge)
+#Plotting the confusion Matrix
+interpret_model(tuned_model)
 
-
-#Quadratic Discriminant Analysis
-qda = create_model('qda')
-predict_model(qda)
-
-
-#Ada Boost Classifier
-ada = create_model('ada')
-predict_model(ada)
-
-#Gradient Boosting Classifier
-gbc = create_model('gbc')
-predict_model(gbc)
-
-#Extra Trees Classifier
-et = create_model('et')
-predict_model(et)
-
-#Light Gradient Boosting Machine
-lightgbm = create_model('lightgbm')
-predict_model(lightgbm)
-
-#Dummy Classifier
-dummy = create_model('dummy')
-predict_model(dummy)
-
-#SVM - Linear Kernel
-svm = create_model('svm')
-predict_model(svm)
-
-#Naive Bayes
-nb = create_model('nb')
-predict_model(nb)
-
+predict_model(xgboost, data = data3)
